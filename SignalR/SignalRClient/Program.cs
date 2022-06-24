c3e1972e94db797df14b586cabe00cb439eab527
AkashSatishYadav <108128604+AkashSatishYadav@users.noreply.github.com>
2022-06-24 16:36:25 +0530

    Add files via upload

---
+++ SignalR/SignalRClient/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.SignalR.Client;
+
+// Build the connection
+HubConnection connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:7131/ChatHub")
+    .WithAutomaticReconnect()
+    .Build();
+
+// Add Event listner
+connection.On<string, string>("Publish Message", (user, message) =>
+{
+    Console.WriteLine($"{user} : {message}");
+});
+
+// start the connection
+connection.StartAsync();
+
+// send the message
+connection.SendAsync("SendData", "Client1", "Hi");
+
+Console.ReadKey();
---
+++ SignalR/SignalRServer/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer.Hubs
+{
+    public class ChatHub :  Hub
+    {
+        public Task SendData(string user, string message)
+        {
+            return Clients.All.SendAsync("Publish Message", user, message);
+        }
+    }
+}
